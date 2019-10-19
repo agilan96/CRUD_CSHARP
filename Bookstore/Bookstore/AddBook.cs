97054abc8ca6ddcb289303d62053e02e2bbfdbe2
agilan96 <53893790+agilan96@users.noreply.github.com>
2019-10-19 19:14:46 +0530

    Create AddBook.cs

---
+++ Bookstore/Bookstore/AddBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    class AddBook : Program
+    {
+        public static void GetBook(int noofbooks)
+        {
+
+            Book book = new Book();
+            Console.Write("Book Id:");
+            book.bookId = int.Parse(Console.ReadLine()); 
+            Console.Write("Book Name:");
+            book.bookName = Console.ReadLine();
+            Console.Write("Book Price:");
+            book.bookPrice = int.Parse(Console.ReadLine());
+            Console.Write("Number of Books:");
+            book.x = book.bookCount = int.Parse(Console.ReadLine());
+            bookList.Add(book);
+
+        }
+    }
+}
