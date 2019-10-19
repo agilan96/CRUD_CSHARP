bb20cdd94138a46139c5950b2d76be624c38e22c
agilan96 <53893790+agilan96@users.noreply.github.com>
2019-10-19 19:15:42 +0530

    Create DeleteBook.cs

---
+++ Bookstore/Bookstore/DeleteBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    class DeleteBook : Program
+    {
+        public static void RemoveBook()
+        {
+            Book book = new Book();
+
+            Console.Write("Enter Book id to be deleted : ");
+
+            int Del = int.Parse(Console.ReadLine());
+
+            if (bookList.Exists(x => x.bookId == Del)) //lambda exp
+            {
+                bookList.RemoveAll(idel => idel.bookId == Del);
+                Console.WriteLine("Book id - {0} has been deleted", Del);
+                Program.sno--;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Book id");
+            }
+
+
+        }
+    }
+}
