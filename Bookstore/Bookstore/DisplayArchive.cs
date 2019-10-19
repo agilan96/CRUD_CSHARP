759e3bc1844bc819a91bb25470efa862dfe010f3
agilan96 <53893790+agilan96@users.noreply.github.com>
2019-10-19 19:15:45 +0530

    Create DisplayArchive.cs

---
+++ Bookstore/Bookstore/DisplayArchive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    class DisplayArchive : Program
+    {
+        public static void Displaylist()
+        {
+            Book book = new Book();
+            Console.WriteLine("S.No" + "  " + "BookId" + "\t" + "BookName" + " \t" + "Price" + "  " + "Qty\n");
+            foreach (Book display in bookList)
+            {
+                Console.WriteLine(Program.sno +"\t"+ display.bookId + "\t" + display.bookName + "\t" + display.bookPrice + "\t" + display.bookCount);
+                Program.sno++;
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
