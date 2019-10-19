e71d5bcc17b3d416ac051ef63f68231eae576001
agilan96 <53893790+agilan96@users.noreply.github.com>
2019-10-19 19:15:36 +0530

    Create BuyDetails.cs

---
+++ Bookstore/Bookstore/BuyDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    
+   
+        class BuyDetails 
+        {
+
+            public int buyBookId;
+            public int buyBookPrice;
+            public string buyBookname;
+            public int buyCount;
+        }
+    
+}
