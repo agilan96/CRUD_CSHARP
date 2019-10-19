a3a9ec552c8abca9421a439d5e4a8c4147dc6e4a
agilan96 <53893790+agilan96@users.noreply.github.com>
2019-10-19 19:15:29 +0530

    Create Book.cs

---
+++ Bookstore/Bookstore/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bookstore
+{
+    class Book 
+    {
+        public int bookId { get; set; }
+        public string bookName { get; set; }
+        public int bookPrice { get; set; }
+        public int bookCount { get; set; }
+        public int x { get; set; }
+        public int y = 1;
+        
+
+    }
+
+   
+
+}
