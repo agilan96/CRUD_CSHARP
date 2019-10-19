0a2bf151da232ad4f53ba4f0d1aa60d727fff876
agilan96 <53893790+agilan96@users.noreply.github.com>
2019-10-19 19:15:38 +0530

    Create Customer.cs

---
+++ Bookstore/Bookstore/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore
+{
+    public class Customer
+    {
+        public int CustId;
+        public string CustName;
+        public string CustAddress;
+        public string buyDate;
+        public int Totalcost;
+        
+    }
+}
